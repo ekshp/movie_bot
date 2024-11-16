24d876570f8d5dfeb2140556c25f444935f02132
ekshp <e.shpetko@gmail.com>
2024-11-16 22:59:21 +0100

    исправленна ошибка поиска по id

--- handlers/idSearcher.js
+++ handlers/idSearcher.js
@@ -5,12 +5,12 @@ export async function searchMovieById(bot, msg) {
   const chatId = msg.chat.id;
   const id = parseInt(msg.text.trim());  
   try {
-      const movie = movieList.find(movie => movie.kinopoisk_id === id);
+    const movie = movieList.find(movies => Number(movies.kinopoisk_id) === id);
       if (movie) {
-          const link = `${movie.name}, ${movie.year} - ${movie.iframe_url}`;
-          await bot.telegram.sendMessage(chatId, `Фильм найден! Вот ваша ссылка: ${link}`);
+        const link = `${movie.name}, ${movie.year} - ${movie.iframe_url}`;
+        await bot.telegram.sendMessage(chatId, `Фильм найден! Вот ваша ссылка: ${link}`);
       } else {
-          await bot.telegram.sendMessage(chatId, 'Извините, кажется это новый фильм и мы еще не успели добавить его в раздачу.');
+        await bot.telegram.sendMessage(chatId, 'Извините, кажется это новый фильм и мы еще не успели добавить его в раздачу.');
       }
   // eslint-disable-next-line no-unused-vars
   } catch (error) {
